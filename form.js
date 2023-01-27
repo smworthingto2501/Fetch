5814bdd7f80d70fbc63a15a8f5c20bc87b176a2f
Sarah Worthington <56094014+smworthingto2501@users.noreply.github.com>
2023-01-26 22:18:30 -0600

    Add files via upload

---
+++ form.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("user-form");
+const occupationSelect = document.getElementById("occupation");
+const stateSelect = document.getElementById("state");
+const formResponse = document.getElementById("form-response");
+
+// Fetch options for occupation and state
+fetch("https://frontend-take-home.fetchrewards.com/form")
+  .then(response => response.json())
+  .then(data => {
+    // Populate occupation options
+    data.occupations.forEach(occupation => {
+      const option = document.createElement("option");
+      option.value = occupation;
+      option.text = occupation;
+      occupationSelect.add(option);
+    });
+
+    // Populate state options
+    data.states.forEach(state => {
+      const option = document.createElement("option");
+      option.value = state.abbreviation;
+      option.text = state.name;
+      stateSelect.add(option);
+    });
+  });
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  // Get form data
+  const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+
+  // Send form data to endpoint
+  fetch("https://frontend-take-home.fetchrewards.com/form", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      if (response.status === 201) {
+        return response.json();
+      } else {
+        throw new Error("Form submission failed");
+      }
+    })
+    .then(responseData => {
+      formResponse.innerHTML = "Form submitted successfully!";
+      console.log(responseData);
+    })
+    .catch(error => {
+      formResponse.innerHTML = error;
+    });
+});
+
